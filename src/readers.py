7ac63b91f3d4d60aef93ad2f797ffef911f6a9a4
Evgeniy Vertianov <vertianov.ev@gmail.com>
2025-05-25 22:48:05 +0700

    add func reader_xlsx_file

--- src/readers.py
+++ src/readers.py
@@ -1,5 +1,6 @@
 import csv
 
+import pandas as pd
 
 
 def reader_csv_file(path_to_file: str) -> list:
@@ -11,12 +12,14 @@ def reader_csv_file(path_to_file: str) -> list:
         return result
 
 
-def reader_xlsx_file(path_to_file: str) -> None:
-    pass
+def reader_xlsx_file(path_to_file: str) -> list:
+    data = pd.read_excel(path_to_file)
+    result = data.to_dict(orient="records")
+    return result
 
 
-if __name__ == "__main__":
-    print(reader_csv_file("/Users/vertianovev/Учеба/Code/StudyProject/data/transactions.csv"))
-
-if __name__ == "__main__":
-    print(reader_xlsx_file("/Users/vertianovev/Учеба/Code/StudyProject/data/transactions_excel.xlsx"))
+# if __name__ == "__main__":
+#     print(reader_csv_file("/Users/vertianovev/Учеба/Code/StudyProject/data/transactions.csv"))
+#
+# if __name__ == "__main__":
+#     print(reader_xlsx_file("/Users/vertianovev/Учеба/Code/StudyProject/data/transactions_excel.xlsx"))
