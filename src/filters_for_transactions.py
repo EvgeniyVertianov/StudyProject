099fb0e0122ad444c5fce1bc10b21a8f46f3196c
Evgeniy Vertianov <vertianov.ev@gmail.com>
2025-05-27 22:17:27 +0700

    add func counting_categories

--- src/filters_for_transactions.py
+++ src/filters_for_transactions.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 def filter_by_state_v2(data: list, user_input: str = "EXECUTED") -> str:
@@ -18,57 +19,102 @@ def filter_by_state_v2(data: list, user_input: str = "EXECUTED") -> str:
         return f'Операции отфильтрованы по статусу "{user_input}"\n{new_list}'
 
 
-def counting_categories():
-    pass
+def counting_categories(data: list, categories_of_operations: list) -> str | dict:
+    """Функция принимает список словарей с данными о банковских операциях и список категорий операций,
+    после чего возвращает словарь, в котором ключи — это названия категорий, а значения — это количество операций
+    в каждой категории."""
+    new_list = []
+    for item in data:
+        item = item.get("description")
+        if item in categories_of_operations:
+            new_list.append(item)
+    result = dict(Counter(new_list))
+    return result
 
 
-if __name__ == "__main__":
-    transact = [
-        {
-            "id": 441945886,
-            "state": "EXECUTED",
-            "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод организации",
-            "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589",
-        },
-        {
-            "id": 441945886,
-            "state": "EXECUTED",
-            "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод организации",
-            "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589",
-        },
-        {
-            "id": 594226727,
-            "state": "CANCELED",
-            "date": "2018-09-12T21:27:25.241689",
-            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод организации",
-            "from": "Visa Platinum 1246377376343588",
-            "to": "Счет 14211924144426031657",
-        },
-        {
-            "id": 939719570,
-            "state": "PENDING",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-        {
-            "id": 939719570,
-            "state": "PENDING",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-    ]
-
-    print(filter_by_state_v2(transact))
+# transact = [
+#         {
+#             "id": 441945886,
+#             "state": "EXECUTED",
+#             "date": "2019-08-26T10:50:58.294041",
+#             "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+#             "description": "Перевод организации",
+#             "from": "Maestro 1596837868705199",
+#             "to": "Счет 64686473678894779589",
+#         },
+#         {
+#             "id": 441945886,
+#             "state": "EXECUTED",
+#             "date": "2019-08-26T10:50:58.294041",
+#             "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+#             "description": "Перевод организации",
+#             "from": "Maestro 1596837868705199",
+#             "to": "Счет 64686473678894779589",
+#         },
+#         {
+#             "id": 594226727,
+#             "state": "CANCELED",
+#             "date": "2018-09-12T21:27:25.241689",
+#             "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+#             "description": "Перевод организации",
+#             "from": "Visa Platinum 1246377376343588",
+#             "to": "Счет 14211924144426031657",
+#         },
+#         {
+#             "id": 939719570,
+#             "state": "PENDING",
+#             "date": "2018-06-30T02:08:58.425572",
+#             "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+#             "description": "Перевод организации",
+#             "from": "Счет 75106830613657916952",
+#             "to": "Счет 11776614605963066702",
+#         },
+#         {
+#             "id": 939719570,
+#             "state": "PENDING",
+#             "date": "2018-06-30T02:08:58.425572",
+#             "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+#             "description": "Перевод организации",
+#             "from": "Счет 75106830613657916952",
+#             "to": "Счет 11776614605963066702",
+#         },
+#         {
+#             "id": 558167602,
+#             "state": "EXECUTED",
+#             "date": "2019-04-12T17:27:27.896421",
+#             "operationAmount": {
+#                 "amount": "43861.89",
+#                 "currency": {
+#                     "name": "USD",
+#                     "code": "USD"
+#                 }
+#             },
+#             "description": "Перевод со счета на счет",
+#             "from": "Счет 73654108430135874305",
+#             "to": "Счет 89685546118890842412"
+#         },
+#         {
+#             "id": 407169720,
+#             "state": "EXECUTED",
+#             "date": "2018-02-03T14:52:08.093722",
+#             "operationAmount": {
+#                 "amount": "67011.26",
+#                 "currency": {
+#                     "name": "руб.",
+#                     "code": "RUB"
+#                 }
+#             },
+#             "description": "Перевод с карты на карту",
+#             "from": "MasterCard 4047671689373225",
+#             "to": "Maestro 3806652527413662"
+#         },
+#     ]
+#
+#
+#
+#
+# if __name__ == "__main__":
+#     print(filter_by_state_v2(transact))
+# if __name__ == "__main__":
+#     categories_of_operations = ['Перевод организации', 'Перевод с карты на карту', 'Перевод с карты на счет', 'Перевод со счета на счет', 'Открытие вклада']
+#     print(counting_categories(transact, categories_of_operations))
